649da1de573eafea6fa6db88a823ce2857e8db11
theshubhamgour <iamtheshubhamgour@gmail.com>
2022-03-20 14:28:37 +0530

    commit

--- CH_7/42#range.py
+++ CH_7/42#range.py
@@ -2,3 +2,7 @@
 
 for i in range(1, 8):
     print(i)
+
+#for skipping to third value in the list
+for i in range(1, 8, 3):
+    print(i)
---
+++ CH_7/44#mul.py
@@ -0,0 +1,4 @@
+# printing  a table
+num = int(input("Enter a number"))
+for i in range(1, 11):
+    print(str(num) + " X " + str(i) + " = " + str(i*num))
